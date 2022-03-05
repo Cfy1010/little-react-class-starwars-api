62bc3f3dc7631597d9231f2fd491d6b90cf27a8d
Cfy1010 <cy_one2000@yahoo.fr>
2022-03-05 15:44:22 +0100

    click event && store data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import React from 'react';
 import './App.css';
 // import Item from './MyItem';
 
-class StarWars extends React.Component{
+class StarWars extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: null,
+      height: null,
+      homeworld: null,
+      films: []
+    }
+  }
+  getNewCharacter() {
+    console.log('new personage');
+    this.setState({
+      name: 'Luke',
+      height: 172,
+      homeworld: 'Tatooine',
+      films: ['film1',' film2']
+    })
+  }
+
   render() {
     return (
-      <h1>C'est StarWars</h1>
-    )
+      <>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.height} cm</p>
+        <p>Homeworld: {this.state.homeworld}</p>
+        <ul>
+          <li>{this.state.films}</li>
+        </ul>
+        <button
+          type='button'
+          onClick={() => this.getNewCharacter()}
+          className='btn'>
+          Change de personnage au hasard
+        </button>
+      </>
+    );
   }
 }
-
 
 function App() {
   return (
